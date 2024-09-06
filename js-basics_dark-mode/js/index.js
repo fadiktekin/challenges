04b2b7d4c9da8f594044253b9ef17a362eb10fb8
Fadik Tekin <tekinfadik@gmail.com>
2024-09-06 11:07:11 +0200

    fix error and toggle bg

--- js-basics_dark-mode/js/index.js
+++ js-basics_dark-mode/js/index.js
@@ -1,3 +1,17 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+darkModeButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
+lightModeButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+const toogleModeButton = document.querySelector('[data-js="toggle-button"]');
+toogleModeButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
