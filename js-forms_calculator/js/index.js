2499f4a14d757a5ac6efe35b64eb2ce9292eecd7
Fadik Tekin <tekinfadik@gmail.com>
2024-09-11 18:53:05 +0200

    solve challenges

--- js-forms_calculator/js/index.js
+++ js-forms_calculator/js/index.js
@@ -22,11 +22,20 @@ function divide(a, b) {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const numberA = Number.parseInt(event.target.elements.numberA.value);
+  const numberB = Number.parseInt(event.target.elements.numberB.value);
+  const operator = event.target.elements.operator.value;
+
   let result;
-
-  // --v-- write your code here --v--
-
-  // --^-- write your code here --^--
+  if (operator === "addition") {
+    result = add(numberA, numberB);
+  } else if (operator == "subtraction") {
+    result = subtract(numberA, numberB);
+  } else if (operator == "multiplication") {
+    result = multiply(numberA, numberB);
+  } else {
+    result = divide(numberA, numberB);
+  }
 
   resultOutput.textContent = result;
 });
