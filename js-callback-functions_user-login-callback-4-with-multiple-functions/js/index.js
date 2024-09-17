74ed41e6f3c148d4601717e9ed5bd25a4581723e
Fadik Tekin <tekinfadik@gmail.com>
2024-09-17 11:15:54 +0200

    callback-challenges

--- js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
+++ js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
@@ -9,3 +9,54 @@ function handleUserLogin(onSuccess, onError, userName) {
 }
 
 // The exercise starts here!
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+
+function showErrorMessage(errorMessage) {
+  console.log(`Login error: ${errorMessage}`);
+}
+
+handleUserLogin(showWelcomeMessage, showErrorMessage, "Jane Doe");
+handleUserLogin(showWelcomeMessage, showErrorMessage, "John Doe");
+
+// Calculating the Total Price
+const products = [
+  { name: "Saffron", price: 50 },
+  { name: "Cinnamon", price: 6 },
+  { name: "Cardamom", price: 12 },
+  { name: "Turmeric", price: 9 },
+  { name: "Paprika", price: 4 },
+  { name: "Garam Masala", price: 10 },
+  { name: "Vanilla Bean", price: 15 },
+  { name: "Coriander", price: 6 },
+];
+
+function extractPrice(product) {
+  return product.price;
+}
+
+function calculateTotalPrice(products, extractPrice) {
+  let totalPrice = 0;
+  for (const product of products) {
+    totalPrice += extractPrice(product);
+  }
+  return totalPrice;
+}
+
+console.log(calculateTotalPrice(products, extractPrice));
+
+//Creating a Custom Map Function
+function double(number) {
+  return number * 2;
+}
+function myMap(numbers, double) {
+  const result = [];
+  for (const number of numbers) {
+    result.push(double(number));
+  }
+  return result;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(myMap(numbers, double));
