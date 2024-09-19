182a7bfdb1b7d9a5e9730606073187f0d5587e5f
Fadik Tekin <tekinfadik@gmail.com>
2024-09-19 11:29:04 +0200

    news flash

--- js-array-methods-2_news-flash/index.js
+++ js-array-methods-2_news-flash/index.js
@@ -5,12 +5,16 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
-});
+const filteredNews = news.filter((newsItem) =>
+  newsItem.categories.includes("politics")
+);
 
 // Part 2 - start here
-const sortedNews = filteredNews;
+const sortedNews = filteredNews.slice().sort((a, b) => {
+  const bodyTextA = a.body;
+  const bodyTextB = b.body;
+  return bodyTextA.length - bodyTextB.length;
+});
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
