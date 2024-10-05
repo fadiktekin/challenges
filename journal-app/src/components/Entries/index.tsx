79a7f545c03c9c6644f2c6a53c75bd7149c4404f
Fadik Tekin <tekinfadik@gmail.com>
2024-10-05 17:35:24 +0200

    show more entries

--- journal-app/src/components/Entries/index.tsx
+++ journal-app/src/components/Entries/index.tsx
@@ -1,11 +1,50 @@
 import { Tabs } from "../Tabs";
-import { EntryList } from "../EntryList";
+import { Entry } from "../Entry";
+
+import "./Entries.css";
+import { Fragment } from "react/jsx-runtime";
+
+const entries = [
+  {
+    id: 1000,
+    date: "Feb 5, 2025",
+    motto: "We are in a state of chaos",
+    notes:
+      "Today I learned about React State. It was fun! I can't wait to learn more.",
+  },
+  {
+    id: 999,
+    date: "Feb 4, 2025",
+    motto: "Props, Props, Props",
+    notes:
+      "Today I learned about React Props. Mad props to everyone who understands this!",
+  },
+  {
+    id: 998,
+    date: "Feb 3, 2025",
+    motto: "How to nest components online fast",
+    notes:
+      "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+  },
+  {
+    id: 997,
+    date: "Feb 2, 2025",
+    motto: "I'm a React Developer",
+    notes: "My React-ion when I learned about React: 😍",
+  },
+];
 
 export function Entries() {
   return (
     <>
       <Tabs />
-      <EntryList />
+      <section className="entry-list">
+        {entries.map((entry) => (
+          <Fragment key={entry.id}>
+            <Entry date={entry.date} motto={entry.motto} notes={entry.notes} />
+          </Fragment>
+        ))}
+      </section>
     </>
   );
 }
--- journal-app/src/components/Entry/index.tsx
+++ journal-app/src/components/Entry/index.tsx
@@ -2,21 +2,17 @@ import "./Entry.css";
 
 import { FavoriteButton } from "../FavoriteButton";
 
-export function Entry() {
-  const now = new Date().toISOString();
+type EntryPropTypes = { date: string; motto: string; notes: string };
 
+export function Entry({ date, motto, notes }: EntryPropTypes) {
   return (
     <article>
-      <time dateTime={now}>{now}</time>
+      <time dateTime={date}>{date}</time>
       <div className="title-icon">
-        <h3>“That’s life in the city“</h3>
+        <h3>{motto}</h3>
         <FavoriteButton />
       </div>
-      <p>
-        Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si
-        mihi. Et quidem se repellere, idque instituit docere sic omne animal,
-        simul atque.
-      </p>
+      <p>{notes}</p>
     </article>
   );
 }
